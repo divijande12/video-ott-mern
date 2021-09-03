3b6dab776cf74343314a0dfc8f7b24882c83d7ca
divijande12 <divijande@rocketmail.com>
2021-09-03 12:22:55 +0530

    final commit

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,12 @@
-// let DOMAIN = "https://popflix-backend.herokuapp.com"
+// // let DOMAIN = "https://popflix-backend.herokuapp.com"
+// let DOMAIN = "http://localhost:5000";
+
+// export { DOMAIN };
+const env = "prod";
+
 let DOMAIN = "http://localhost:5000";
+if (env === "prod") {
+  DOMAIN = "https://popflix-backend.herokuapp.com";
+}
 
 export { DOMAIN };
